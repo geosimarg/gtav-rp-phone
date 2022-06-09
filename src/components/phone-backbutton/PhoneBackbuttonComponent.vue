ebffe72b6cfca13af56d9b0163be7f453b46318c
Geosimar G <geosimar@gmail.com>
2022-06-09 14:10:15 -0300

    Fix in backbutton

--- src/components/phone-backbutton/PhoneBackbuttonComponent.vue
+++ src/components/phone-backbutton/PhoneBackbuttonComponent.vue
@@ -1,10 +1,14 @@
 <template src="./index.html"></template>
 
 <script lang="ts">
+import router from '@/router';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class PhoneBackbuttonComponent extends Vue {
+  GoToHome(): void {
+    router.back();
+  }
 }
 </script>
 

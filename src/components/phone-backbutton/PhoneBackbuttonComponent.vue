7b0e48672d4ffa34ee961fc2d24d34bfdd070c8c
Geosimar G <geosimar@gmail.com>
2022-06-09 21:34:37 -0300

    phone-backbutton theme dark/ligth

--- src/components/phone-backbutton/PhoneBackbuttonComponent.vue
+++ src/components/phone-backbutton/PhoneBackbuttonComponent.vue
@@ -2,10 +2,11 @@
 
 <script lang="ts">
 import router from '@/router';
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class PhoneBackbuttonComponent extends Vue {
+  @Prop({ default: true }) public white!: boolean;
   GoToHome(): void {
     router.back();
   }
